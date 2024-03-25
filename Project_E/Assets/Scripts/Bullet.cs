a3ad4537369a9461ec151079dfa5efd6e7d41a3e
Cyuy_ <1615936638@qq.com>
2024-03-25 11:06:34 +0800

    Update Bullet.cs

--- Project_E/Assets/Scripts/Bullet.cs
+++ Project_E/Assets/Scripts/Bullet.cs
@@ -1,19 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Security.Cryptography;
-=======
->>>>>>> origin/dev
 using Attacker;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/dev
     public int damage;
     public float speed;
     private Transform target;
@@ -23,10 +16,6 @@ public class Bullet : MonoBehaviour
     {
         this.target = _target;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/dev
     // Start is called before the first frame update
     void Start()
     {
@@ -38,16 +27,6 @@ public class Bullet : MonoBehaviour
     {
         transform.LookAt(target.position);
         transform.Translate( Time.deltaTime * speed *Vector3.forward);
-    }
-    //用Collider和RigidBody检测子弹是否击中敌人
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Enemy")
-        {
-            other.GetComponent<AttackerBase>().GetDamage(damage);
-            GameObject.Instantiate(ExplosionEffectPrefab, transform.position, transform.rotation);
-            Destroy(this.gameObject);
-        }
     }
     //用触发器检测碰撞（加上collider和rigidbody）
     private void OnTriggerEnter(Collider other)
